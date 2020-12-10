5a65af892bb68c9abed572bda76e0c6e5f6c73ab
Ali123-gig <ali99shair@gmail.com>
2020-12-10 13:49:05 +0500

    done with initial setup of web skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { Route ,Switch } from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import Authentication from './Pages/Authentication/Authentication';
+import Catgeory from './Pages/Category/Catgeory';
+import CategoryProducts from './Pages/CategoryProducts/CategoryProducts';
+import CheckOut from './Pages/CheckOut/CheckOut';
 
 function App() {
   return (
     <div className="App">
-     <h1>My React App</h1>
+     
+        <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/Authentication" component={Authentication}/>
+          <Route path="/Category" component={Catgeory}/>
+          <Route path="/CategoryProducts" component={CategoryProducts}/>
+          <Route path="/CheckOut" component={CheckOut}/>
+        </Switch>
+    
     </div>
   );
 }
---
+++ src/Pages/Category/Catgeory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Catgeory = () => {
+    return (
+        <div>
+            <h1>Category
+            </h1>
+        </div>
+    )
+}
+
+export default Catgeory
