df9d44bc99780de6355a2169b4aa2f570f0482eb
shair1020 <ali99shair@gmail.com>
2021-03-19 13:27:48 -0700

    final test

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -3,29 +3,24 @@ import CheckoutList from "../../Components/CheckoutList/CheckoutList";
 import { connect } from "react-redux";
 import { calculateTotal } from "./../../utility/Checkout";
 import OrderForm from "../../Components/OrderForm/OrderForm";
-import "./CheckOut.css"
+import "./CheckOut.css";
 
 const CheckOut = ({ total }) => {
   const [shipFormShown, setShipFormShown] = useState(false);
   return (
     <div className="check-out">
       <div className="check">
-  <h1>Checkout</h1>
-      <CheckoutList />
-      <h2>Total Amount:{total} </h2>
-      {/* {shipFormShown && <OrderForm />}
+        <h1>Checkout</h1>
+        <CheckoutList />
+        <h2>Total Amount:{total} </h2>
+        {/* {shipFormShown && <OrderForm />}
       <button onClick={()=>setShipFormShown(!shipFormShown)}>PROCEED & PAY</button> */}
       </div>
-    
     </div>
   );
 };
 var mapState = (state) => ({
   total: calculateTotal(state.cart),
-
-
-
-  
 });
 
 export default connect(mapState)(CheckOut);
